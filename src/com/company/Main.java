add40005d7491d213a4104d409009a07daf44a3e
XXX <joxlaq10@gmail.com>
2020-06-04 10:31:06 +0300

    Some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,12 +11,12 @@ public class Main {
                 new Division(new Pow(2, new Linear(1)), new Constant(2))),
                 new Division(new Pow(3, new Linear(1)), new Constant(6))),
                 new Division(new Pow(4, new Linear(1)), new Constant(24)));
-        System.out.println("F1="+F1.calculate(x));
-        System.out.println("F1="+F1.derivative().calculate(x));
+        System.out.println("f1="+F1.calculate(x));
+        System.out.println("f1="+F1.derivative().calculate(x));
         Function F2 = new Multiplication(new Linear(1),
                 new Sum(new Sin(new Pow(3, new Linear(1))),
                         new Pow(2, new Cos(new Constant(y)))));
-        System.out.println("F2="+F2.calculate(x));
-        System.out.println("F2="+F2.derivative().calculate(x));
+        System.out.println("f2="+F2.calculate(x));
+        System.out.println("f2="+F2.derivative().calculate(x));
     }
 }
